083558cfb009cfbb0f886e052ba592707e84ad75
rlatneorp <rlatneorp@gmail.com>
2024-07-10 18:43:02 +0900

    feat : 배열 두배 만들기

--- src/programmers/배열두배만들기/Solution.java
+++ src/programmers/배열두배만들기/Solution.java
@@ -1,2 +1,13 @@
-package programmers.배열두배만들기;public class Solution {
+package programmers.배열두배만들기;
+
+//  https://school.programmers.co.kr/learn/courses/30/lessons/120809
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            answer[i] = numbers[i] * 2;
+        }
+        return answer;
+    }
 }
--- src/programmers/짝수홀수개수/Solution.java
+++ src/programmers/짝수홀수개수/Solution.java
@@ -1,2 +1,18 @@
-package programmers.짝수홀수개수;public class Solution {
+package programmers.짝수홀수개수;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/120824
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = {0, 0};
+        for (int i = 0; i < num_list.length; i++) {
+            if(num_list[i] % 2 == 0){
+                answer[0]++;
+            }
+            if(num_list[i] % 2 > 0){
+                answer[1]++;
+            }
+        }
+        return answer;
+    }
 }
