e9ff7b68d63ff9cc49abe7173d25e1b9ab410574
rlatneorp <rlatneorp@gmail.com>
2024-07-11 09:38:39 +0900

    feat : 짝수는 싫어요

---
+++ src/programmers/짝수는싫어요/Solution.java
@@ -0,0 +1,14 @@
+package programmers.짝수는싫어요;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/120813
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = {};
+        for (int i = 1; i <= n; i++) {
+
+        }
+
+        return answer;
+    }
+}
