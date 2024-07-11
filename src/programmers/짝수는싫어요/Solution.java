768cb7fb1b2c11bee01ac6313a6f5c3fcd0f7a74
rlatneorp <rlatneorp@gmail.com>
2024-07-11 19:03:36 +0900

    feat : 짝수는 싫어요

--- src/programmers/짝수는싫어요/Solution.java
+++ src/programmers/짝수는싫어요/Solution.java
@@ -2,13 +2,22 @@ package programmers.짝수는싫어요;
 
 //https://school.programmers.co.kr/learn/courses/30/lessons/120813
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public int[] solution(int n) {
-        int[] answer = {};
+        List<Integer> list = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
-
+            if(i % 2 != 0) {
+                list.add(i);
+            }
         }
 
-        return answer;
+        int[] result = new int[list.size()];
+        for(int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+        return result;
     }
 }
